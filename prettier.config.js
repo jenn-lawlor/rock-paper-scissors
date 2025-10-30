88a248a5a4e8721c223332b4a36d8312d444e982
Jenn Lawlor <jenn@midnightsun.ai>
2025-10-30 14:38:39 -0800

    feat: add prettier.config.js and settings.json

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// You can modify this to change editor behavior!
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto", // ensures prettier doesn't collapse single newlines
+};
+
+export default config;
